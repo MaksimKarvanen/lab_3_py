73646063b4c89e70a51f96968542758c495bbd7c
Maksim <maksimkarvanen03@gmail.com>
2025-04-07 22:39:34 +0300

    Добавил комментарии

--- classes.py
+++ classes.py
@@ -1,74 +1,83 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod # Импорт абстрактного базового класса и декоратора abstractmethod из модуля abc
 import math
 
 #-------------------------------A-------------------------------
 class Person:
+    # Конструктор класса, инициализирует объект с именем и возрастом
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        # Публичные атрибуты объекта
+        self.name = name # Имя
+        self.age = age # Возраст
 
+    # Метод для получения имени
     def getName(self):
-        return self.name
+        return self.name # Возвращает текущее значение поля name
     
+    # Метод  для установки нового имени
     def setName(self, name):
         self.name = name
 
     def printInfo(self):
-        print(f"Имя: {self.name}, Возраст: {self.age}")
+        print(f"Имя: {self.name}, Возраст: {self.age}") # f-строка для форматированного вывода
 
 
 #-------------------------------B-------------------------------
+# Класс Student, наследующий от Person
 class Student(Person):
     def __init__(self, name, age, studentID):
-        super().__init__(name, age)
-        self.studentID = studentID
+        super().__init__(name, age) # Вызов конструктора родительского класса
+        self.studentID = studentID # Добавление нового атрибута StudentID
 
     def getStudentID(self):
         return self.studentID
     
-    def printInfo(self):
-        super().printInfo()
+    # Переопределенный метод вывода информации
+    def printInfo(self): 
+        super().printInfo() # Вызываем метод родительского класса
         print(f"Student ID: {self.studentID}")
 
 
 #-------------------------------C-------------------------------
 class Shape(ABC):
+    # Абстрактный метод, который должны реализовать все наследники
     @abstractmethod
     def calculateArea(self):
-        pass
+        pass # Заглушка
 
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.width = width # Широта
+        self.height = height # Высота
 
     def calculateArea(self):
-        return self.width * self.height
+        return self.width * self.height # Возвращаем площадь
     
 
 class Circle(Shape):
     def __init__(self, radius):
-        self.radius = radius
+        self.radius = radius # Радиус
 
     def calculateArea(self):
-        return math.pi * self.radius ** 2
+        return math.pi * self.radius ** 2 # площадь окружности - pi * R^2
         
         
 #-------------------------------D-------------------------------
 class Account:
     def __init__(self):
-        self.__balance = 0
+        self.__balance = 0 # Приватное поле баланса (изначально 0)
 
+    # Пополнение
     def deposit(self, amount):
-        if amount > 0:
-            self.__balance += amount
-
+        if amount > 0: # Проверка, что сумма положительна
+            self.__balance += amount # Увеличиваем баланс
+    
+    # Снятие
     def withdraw(self, amount):
         if 0 < amount <= self.__balance:
             self.__balance -= amount
         else:
-            print("Ошибка вывода")
+            print("Ошибка вывода") # Если снятие превышает лимит
 
     def getBalance(self):
         return self.__balance
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from classes import *
 
-person = Person("Максим", 21)
-print(person.getName())
-person.setName("Павел")
-person.printInfo()
+person = Person("Максим", 21) # Создание объекта класса Person
+print(person.getName()) # Вывод имени
+person.setName("Павел") # Изменение имени
+person.printInfo() # Вывод полной информации
 print()
 
-student = Student("Карванен", 22, "KMD21")
-student.printInfo()
-print(student.getStudentID())
+student = Student("Карванен", 22, "KMD21") # Создание объекта класса Student
+student.printInfo() 
+print(student.getStudentID()) # Вывод studentID
 print()
 
-shapes = [Rectangle(4, 5), Circle(3)]
-for shape in shapes:
-    print(f"Area: {shape.calculateArea():.2f}")
+shapes = [Rectangle(4, 5), Circle(3)] # Создание списка геометрических фигур
+for shape in shapes: # Итерация по списку фигур
+    print(f"Area: {shape.calculateArea():.4f}") # Для каждой фигуры выводим площадь с округлением до 4 знаков
 print()
 
-account = Account()
-account.deposit(100)
-account.withdraw(50)
-print("Баланс: ", account.getBalance())
-account.withdraw(100)
+account = Account() # Создание счета
+account.deposit(100) # Вносим 100
+account.withdraw(50) # Снимаем 50 
+print("Баланс: ", account.getBalance()) # Выводим текущий баланс
+account.withdraw(100) # Попытка снять больше, чем есть на счету
